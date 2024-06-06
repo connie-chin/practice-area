a1e3da5f8d9b4b8cc913ec0c28e56201d572f002
Connie Chin <conniec96@gmail.com>
2024-06-06 23:06:16 +0000

    Practice string methods.

---
+++ javascript-string-methods/main.js
@@ -0,0 +1,9 @@
+const name = 'Georgie Anne Chin';
+console.log(name.indexOf('g'));
+console.log(name.lastIndexOf('i'));
+console.log(name.repeat(3));
+console.log(name.replace('Chin', 'Nguyen'));
+console.log(name.slice(8));
+console.log(name.slice(-4)); //create new word or phrase, doesnt modify original string
+console.log(name.split(' ')); //array
+//trim
