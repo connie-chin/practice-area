bb5914f6a96396bf68bc71851bf0dd8d3d5b49b9
Connie Chin <conniec96@gmail.com>
2024-07-06 19:50:26 +0000

    Learned difference between classList and className with light switch toggle.

---
+++ typescript-light-bulb/main.ts
@@ -0,0 +1,22 @@
+const $lightBulb = document.querySelector('.light-bulb');
+const $container = document.querySelector('.container');
+if (!$lightBulb || !$container)
+  throw new Error('This $lightBulb or $container query failed');
+// let isOn = true;
+
+$lightBulb.addEventListener('click', () => {
+  $container.classList.toggle('container-off');
+  $lightBulb.classList.toggle('light-bulb-off');
+}); // with this, only have css for changing colors. less code for sure!
+
+// $lightBulb.addEventListener('click', () => {
+//   isOn = !isOn;
+//   if (isOn === false){
+//   $container.className='container-off';
+//   $lightBulb.className='light-bulb-off';
+//   }
+//   if (isOn === true) {
+//     $container.className = 'container';
+//     $lightBulb.className='light-bulb';
+//   }
+// }) //with this route, need to have matching css for both on and off.
