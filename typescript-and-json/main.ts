06cce1e4ba6a27fcb1439e96f1093656cbed47b9
Connie Chin <conniec96@gmail.com>
2024-07-10 22:20:13 +0000

    Practiced JSON.

---
+++ typescript-and-json/main.ts
@@ -0,0 +1,25 @@
+interface Books {
+  isbn: string;
+  title: string;
+  author: string;
+}
+
+const bookArr: Books[] = [
+  { isbn: 'abc123', title: 'Cat in Hat', author: 'Dr. Seuss' },
+  { isbn: 'def456', title: 'When Harry met Sally', author: 'Tom Hanks' },
+  { isbn: 'ghi789', title: 'She is the Man', author: 'Amanda Bynes' },
+];
+
+console.log('bookArr:', bookArr);
+console.log('typeof bookArr:', typeof bookArr);
+
+console.log('JSON stringify bookArr:', JSON.stringify(bookArr)); // turns JavaScript value to JSON string
+console.log('JSON stringify bookArr typeof:', typeof JSON.stringify(bookArr));
+
+const student: string = '{"numberId":"012", "String Name":"George Kind"}'; // string we want to turn into JSON object
+console.log('student:', student); // just remember this format for JSON^^
+console.log('typeof student:', typeof student);
+
+const newObj = JSON.parse(student); // created a JSON object from our string
+console.log('newObj:', newObj);
+console.log('typeof newObj:', typeof newObj);
---
+++ typescript-and-json/typescript-and-json-quiz-notes.md
@@ -0,0 +1,43 @@
+# typescript-and-json-quiz-notes
+
+## Quiz Questions
+
+Answer the following questions in the provided markdown file before turning in this exercise:
+
+- What is JSON?
+
+- What are serialization and deserialization?
+  deserialization is when you convert JSON string to object, serialization is when you convert object into JSON string
+- Why are serialization and deserialization useful?
+
+- How do you serialize a data structure into a JSON string using JavaScript?
+
+- How do you deserialize a JSON string into a data structure using JavaScript?
+
+## Notes
+
+All student notes should be written here.
+
+How to write `Code Examples` in markdown
+
+for JS:
+
+```javascript
+const data = 'Howdy';
+```
+
+for HTML:
+
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+
+```css
+div {
+  width: 100%;
+}
+```
