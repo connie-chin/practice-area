a9fe8c42eafe8c32592055a5d6cc23cbba6de365
Connie Chin <conniec96@gmail.com>
2024-07-16 02:08:22 +0000

    Got images and dots to cycle automatically.

---
+++ typescript-carousel/main.ts
@@ -0,0 +1,30 @@
+const $carouselImage = document.querySelectorAll('.carousel-image');
+const $dots = document.querySelectorAll('.fa-circle');
+
+if (!$carouselImage || !$dots)
+  throw new Error('This $carouselImage or $dots query failed');
+console.log('$carouselImage:', $carouselImage);
+console.log('$dots:', $dots);
+
+function updateCarousel(): void {
+  for (let i = 0; i < $carouselImage.length; i++) {
+    if (index === i) {
+      $carouselImage[i].classList.remove('hidden');
+      $dots[i].classList.add('fa-solid');
+      $dots[i].classList.remove('fa-regular');
+    } else {
+      $carouselImage[i].classList.add('hidden');
+      $dots[i].classList.remove('fa-solid');
+      $dots[i].classList.add('fa-regular');
+    }
+  }
+}
+
+let index = 0;
+updateCarousel();
+setInterval(() => {
+  index = (index + 1) % $carouselImage.length;
+  updateCarousel();
+}, 3000);
+
+// able to loop through images automatically right now. still need to understand a little more.
